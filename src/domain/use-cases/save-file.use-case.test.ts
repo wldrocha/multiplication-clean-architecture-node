4a1db46c5515130deebadbca0970daf2a8521d6f
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-13 11:39:26 -0400

    test: 🧪 test to error when file not be created

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -4,12 +4,12 @@ import { SaveFile } from './save-file.use-case'
 describe('SaveFileUseCase', () => {
   const saveFile = new SaveFile()
 
-  const options = {
+  const customOptions = {
     fileContent: 'Custom content',
     fileDestination: 'custom-outputs',
     fileName: 'custom-table-name'
   }
-  const filePath = `${options.fileDestination}/${options.fileName}.txt`
+  const filePath = `${customOptions.fileDestination}/${customOptions.fileName}.txt`
 
   afterEach(() => {
     const isOutputsExist = fs.existsSync('outputs')
@@ -31,13 +31,37 @@ describe('SaveFileUseCase', () => {
   })
 
   test('should save file with custom values', () => {
-    const result = saveFile.execute(options)
+    const result = saveFile.execute(customOptions)
     const isFileExist = fs.existsSync(filePath)
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
 
     expect(result).toBe(true)
     expect(isFileExist).toBe(true)
-    expect(fileContent).toBe(options.fileContent)
-    fs.rmSync(options.fileDestination, { recursive: true })
+    expect(fileContent).toBe(customOptions.fileContent)
+    fs.rmSync(customOptions.fileDestination, { recursive: true })
+  })
+
+  test('should return false when directory could not be created', () => {
+    // overwrite the mkdirSync method to throw an error
+    const mkdirSpy = jest.spyOn(fs, 'mkdirSync').mockImplementation(() => {
+      throw new Error('Error creating directory')
+    })
+
+    const result = saveFile.execute(customOptions)
+    expect(result).toBeFalsy()
+
+    //restore the original method
+    mkdirSpy.mockRestore()
+  })
+  test('should return false when file could not be created', () => {
+    // overwrite the mkdirSync method to throw an error
+    const writeFileSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {
+      throw new Error('Error creating file')
+    })
+    const result = saveFile.execute({ fileContent: 'Hello' })
+    expect(result).toBeFalsy()
+
+    //restore the original method
+    writeFileSpy.mockRestore()
   })
 })
