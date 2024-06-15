f6737c6aaba47c2b544bf9a039fdc186887de9d6
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-14 20:34:58 -0400

    test: 🧪 update yargs initialization to use hideBin helper

--- src/config/plugins/args.plugin.ts
+++ src/config/plugins/args.plugin.ts
@@ -1,7 +1,7 @@
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
-export const yarg = yargs(process.argv)
+export const yarg = yargs(hideBin(process.argv))
   .option('b', {
     alias: 'base',
     type: 'number',
