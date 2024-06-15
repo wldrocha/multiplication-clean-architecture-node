57d844d76de012db68629d5f49e003403604edca
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-14 22:14:44 -0400

    ♻️: Add serverApp test for running with options

--- src/presentation/server-app.test.ts
+++ src/presentation/server-app.test.ts
@@ -1,3 +1,4 @@
+import { CreateTable, SaveFile } from '../domain'
 import { ServerApp } from './server-app'
 
 describe('ServerApp', () => {
@@ -6,5 +7,29 @@ describe('ServerApp', () => {
 
     expect(serverApp).toBeInstanceOf(ServerApp)
     expect(typeof ServerApp.run).toBe('function')
+  })
+
+  test('should run serverApp with options', () => {
+    const logSpy = jest.spyOn(console, 'log')
+    const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute')
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute')
+    const options = {
+      base: 2,
+      limit: 10,
+      isShowTable: true,
+      fileName: 'test-name',
+      fileDestination: 'test-destination'
+    }
+    ServerApp.run(options)
+
+    expect(logSpy).toHaveBeenCalledTimes(3)
+    expect(logSpy).toHaveBeenCalledWith('🚀 Server is running...')
+    expect(logSpy).toHaveBeenCalledWith('File created!')
+    expect(createTableSpy).toHaveBeenCalledTimes(1)
+    expect(createTableSpy).toHaveBeenCalledWith({ base: 2, limit: 10 })
+    expect(saveFileSpy).toHaveBeenCalledTimes(1)
+
+
+    // expect
   })
 })
