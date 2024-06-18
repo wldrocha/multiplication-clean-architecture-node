0c0b553b6d4616cc60027130461960b1f81d9282
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-17 22:18:50 -0400

    test: ✅ update serverApp test for running with options

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,19 @@
-describe('App', () => {
-    test('should run', () => {
-        expect(true).toBe(true)
+import { ServerApp } from './presentation'
+
+describe('Test App.ts', () => {
+  test('should call Server.run with values', async() => {
+    const serverRunMock = jest.fn()
+    ServerApp.run = serverRunMock
+    process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-destination']
+
+    await import('./app')
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      isShowTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination'
     })
+  })
 })
